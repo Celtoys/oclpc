0aac1f6a2fee19b7b1984149d75bb914cac4289e
Don Williamson <dwilliamson_coder@hotmail.com>
2014-06-05 14:51:22 +0100

    oclpc: Fix win32 libs not being found for build.

--- build_win.bat
+++ build_win.bat
@@ -1,27 +1,27 @@
 
 @echo off
 
-REM
-REM Copyright 2014 Celtoys Ltd
-REM
-REM Licensed under the Apache License, Version 2.0 (the "License");
-REM you may not use this file except in compliance with the License.
-REM You may obtain a copy of the License at
-REM
-REM     http://www.apache.org/licenses/LICENSE-2.0
-REM
-REM Unless required by applicable law or agreed to in writing, software
-REM distributed under the License is distributed on an "AS IS" BASIS,
-REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-REM See the License for the specific language governing permissions and
-REM limitations under the License.
-REM
+::
+:: Copyright 2014 Celtoys Ltd
+::
+:: Licensed under the Apache License, Version 2.0 (the "License");
+:: you may not use this file except in compliance with the License.
+:: You may obtain a copy of the License at
+::
+::     http://www.apache.org/licenses/LICENSE-2.0
+::
+:: Unless required by applicable law or agreed to in writing, software
+:: distributed under the License is distributed on an "AS IS" BASIS,
+:: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+:: See the License for the specific language governing permissions and
+:: limitations under the License.
+::
 
 setlocal
 
-REM
-REM Search for the location of Visual Studio
-REM
+::
+:: Search for the location of Visual Studio
+::
 if DEFINED VS110COMNTOOLS (
 	set "VS_TOOLS_DIR=%VS110COMNTOOLS%"
 ) else if DEFINED VS100COMNTOOLS (
@@ -36,9 +36,9 @@ if DEFINED VS110COMNTOOLS (
 )
 
 
-REM
-REM Search for the location of OpenCL in all major distributions
-REM
+::
+:: Search for the location of OpenCL in all major distributions
+::
 if DEFINED CUDA_PATH (
 	set "OPENCL_INCLUDE_DIR=%CUDA_PATH%\include"
 	set "OPENCL_LIBRARY_DIR=%CUDA_PATH%\lib\Win32"
@@ -54,14 +54,30 @@ if DEFINED CUDA_PATH (
 )
 
 
-REM
-REM Apply environment necessary to use cl.exe
-REM
+::
+:: Apply environment necessary to use cl.exe
+::
 set VC_DIR=%VS_TOOLS_DIR%..\..\VC
 call "%VC_DIR%\vcvarsall.bat"
 
 
-REM
-REM Quick and dirty compile of the single file
-REM
-cl.exe src/oclpc.c /nologo /I "%OPENCL_INCLUDE_DIR%" /Fobin/oclpc.obj /link /LIBPATH:"%OPENCL_LIBRARY_DIR%" /OUT:bin/oclpc.exe opencl.lib
+::
+:: Search for the windows SDK
+::
+set KEY_NAME="HKLM\SOFTWARE\Microsoft\Microsoft SDKs\Windows"
+set VALUE_NAME=CurrentInstallFolder
+for /F "usebackq skip=2 tokens=1,2*" %%A in (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) do (
+	set "ValueName=%%A"
+	set "ValueType=%%B"
+	set WINDOWS_SDK_DIR=%%C
+)
+if not defined WINDOWS_SDK_DIR (
+	echo %KEY_NAME%\%VALUE_NAME% not found.
+	exit
+)
+
+
+::
+:: Quick and dirty compile of the single file
+::
+cl.exe src/oclpc.c /nologo /I "%OPENCL_INCLUDE_DIR%" /Fobin/oclpc.obj /link /LIBPATH:"%OPENCL_LIBRARY_DIR%" /LIBPATH:"%WINDOWS_SDK_DIR%lib" /OUT:bin/oclpc.exe opencl.lib
